e50c74f57a15a77171a5a55f079f9058d206673f
Kamil Karpacz <kamil.karpacz@thecodebrothers.pl>
2023-05-23 23:16:32 +0200

    feat: example app

---
+++ example/lib/main.dart
@@ -0,0 +1,211 @@
+import 'package:example/repositories/mock_calendar_events_repository.dart';
+import 'package:example/services/mock_events_service.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:simple_calendar/simple_calendar.dart';
+
+class CalendarState {}
+
+void main() async {
+  final MockEventsService eventsService = MockEventsService();
+  final MockCalendarEventsRepository calendarRepository =
+      MockCalendarEventsRepository(eventsService);
+
+  // if you want to use 'locale' parameter in calendar widgets
+  // and do not have provided GlobalMaterialLocalizations.delegate
+  // you should call it at least once at the beginning of your app
+  await initializeDateFormatting();
+
+  runApp(MyApp(
+    calendarRepository: calendarRepository,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+    required this.calendarRepository,
+  });
+
+  final CalendarEventsRepository calendarRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Calendar Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        calendarRepository: calendarRepository,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+    required this.calendarRepository,
+  });
+
+  final CalendarEventsRepository calendarRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Simple Calendar"),
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Text(
+                    "One Day Calendar",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Tab(
+                  icon: Text(
+                    "Many-days Calendar",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Tab(
+                  icon: Text(
+                    "Month Calendar",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              OneDayCalendarTab(calendarRepository: calendarRepository),
+              MultipleDaysCalendarTab(calendarRepository: calendarRepository),
+              MonthCalendarTab(calendarRepository: calendarRepository)
+            ],
+          )),
+    );
+  }
+}
+
+class OneDayCalendarTab extends StatelessWidget {
+  OneDayCalendarTab({
+    super.key,
+    required this.calendarRepository,
+  });
+
+  final ScrollController scrollController = ScrollController();
+  final CalendarEventsRepository calendarRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: OneDayCalendarView(
+        scrollController: scrollController,
+        calendarEventsRepository: calendarRepository,
+        // Optional locale for translations
+        // locale: <your current locale>,
+
+        // Optionally, you can customize calendar settings - text styles, etc.
+        // calendarSettings: <CalendarSettings>,
+
+        // Here you can pass additional optional day labels
+        // that will be shown after the date in the header.
+        // Mostly used for translations but you can also show some custom labels
+        tomorrowDayLabel: (_) => 'Tomorrow',
+        todayDayLabel: (_) => 'Today',
+        yesterdayDayLabel: (_) => 'Yesterday',
+
+        // Optional callbacks
+        onEventTap: (event) => ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(SnackBar(content: Text("${event.singleLine} tapped"))),
+        onLongPress: (date) => ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+              SnackBar(content: Text("Hour ${date.hour} long pressed"))),
+      ),
+    );
+  }
+}
+
+class MultipleDaysCalendarTab extends StatelessWidget {
+  MultipleDaysCalendarTab({
+    super.key,
+    required this.calendarRepository,
+  });
+
+  final CalendarEventsRepository calendarRepository;
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: MultipleDaysCalendarView(
+            scrollController: scrollController,
+            calendarEventsRepository: calendarRepository,
+
+            // Optionally, you can customize calendar settings - text styles, etc.
+            // calendarSettings: <CalendarSettings>,
+
+            // Optional locale for translations
+            // locale: <your current locale>,
+
+            // Optional callbacks
+            onTap: (event) => ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                  SnackBar(content: Text("${event.singleLine} tapped"))),
+            onLongPress: (date) => ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                  SnackBar(content: Text("Day ${date.day} long pressed")))));
+  }
+}
+
+class MonthCalendarTab extends StatelessWidget {
+  MonthCalendarTab({
+    super.key,
+    required this.calendarRepository,
+  });
+
+  final CalendarEventsRepository calendarRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: MonthCalendarView(
+      calendarEventsRepository: calendarRepository,
+      onSelected: (date) => ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(SnackBar(content: Text("Day ${date.day} tapped"))),
+
+      // Optional locale for translations
+      // locale: <your current locale>,
+
+      // Optionally, you can customize weekdays abbreviations
+      // By default there will be default abbreviations for your locale
+      // customWeekdayAbbreviation: (context, dayNumber){
+      //   final locale = Localizations.localeOf(context);
+      //
+      //   switch(dayNumber){
+      //     case DateTime.monday: return 'MON';
+      //     case DateTime.tuesday: return 'TUE';
+      //     case DateTime.wednesday: return 'WED';
+      //     case DateTime.thursday: return 'THU';
+      //     case DateTime.friday: return 'FRI';
+      //     case DateTime.saturday: return 'SAT';
+      //     case DateTime.sunday: return 'SUN';
+      //     default: return null;
+      //   }
+      // },
+
+      // Optionally, you can customize calendar settings - text styles, etc.
+      // calendarSettings: <CalendarSettings>,
+    ));
+  }
+}
---
+++ example/lib/repositories/mock_calendar_events_repository.dart
@@ -0,0 +1,49 @@
+import 'package:example/models/event_item.dart';
+import 'package:example/services/mock_events_service.dart';
+import 'package:flutter/material.dart';
+import 'package:simple_calendar/simple_calendar.dart';
+
+class MockCalendarEventsRepository extends CalendarEventsRepository {
+  MockCalendarEventsRepository(this._service);
+
+  final MockEventsService _service;
+
+  @override
+  Future<List<SingleCalendarEvent>> getEventsForDay(DateTime date) async {
+    final events = await _service.getEventsForDay(date);
+
+    return _mapEvents(events);
+  }
+
+  @override
+  Future<List<SingleCalendarEvent>> getEventsForMultipleDays(
+      DateTime fromDate, DateTime toDate) async {
+    final fixedFromDate = DateTime(fromDate.year, fromDate.month, fromDate.day);
+    final fixedToDate = DateTime(toDate.year, toDate.month, toDate.day);
+
+    final events =
+        await _service.getEventsForMultipleDays(fixedFromDate, fixedToDate);
+
+    return _mapEvents(events);
+  }
+
+  Future<List<SingleCalendarEvent>> _mapEvents(List<EventItem> events) async {
+    return events
+        .map(
+          (e) => SingleCalendarEvent(
+            id: e.id,
+            singleLine: e.name,
+            isAllDay: e.isAllDay,
+            eventStart: e.eventStart,
+            eventEnd: e.eventEnd,
+            object: e,
+            localIconName: '',
+            networkIconName: '',
+            iconBackgroundColor: Colors.cyan,
+            dotTileColor: Colors.transparent,
+            tileBackgroundColor: Colors.white,
+          ),
+        )
+        .toList();
+  }
+}
